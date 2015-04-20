ea6fd48510ea793b902d0cef5f3d6a919b300130
Blurredbits <morris00@mac.com>
2015-04-19 20:06:36 -0600

    Added new player view / form

--- main.rb
+++ main.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
+class Player < ActiveRecord::Base
+
+end
+
 get '/' do
   erb :home
+end
+
+get '/players/new' do
+  @player = Player.new
+  erb :new_player
+end
+
+post '/players' do
+  @player = Player.new(params[:player])
+  if @player.save
+    erb :home
+  else
+    erb :new_player
+  end
+end
+
+get '/admin/players' do
+  @players = Player.all
 end
